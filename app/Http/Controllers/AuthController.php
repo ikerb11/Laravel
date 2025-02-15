4f6f1bbde83f5a6db2402565f8db74e9cd170c31
ikerb11 <ikerberna0@gmail.com>
2025-02-15 21:12:32 +0100

    Auth terminada

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('name', 'password');
+    
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            
+            if (!$user instanceof User) {
+                $user = new User();
+            }
+            // Crear un token solo si el usuario existe
+            if ($user) {
+                $token = $user->createToken('auth_token')->plainTextToken;
+                return response()->json(['token' => $token, 'user' => $user], 200);
+            }
+        }
+    
+        return response()->json(['message' => 'Credenciales incorrectas'], 401);
+    }
+    public function userProfile(Request $request)
+    {
+        return response()->json(['user' => Auth::user()], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Sesión cerrada correctamente'], 200);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AlumnoControler;
 use App\Http\Controllers\ProfesorController;
 use App\Http\Controllers\AsignaturaController;
+use App\Http\Controllers\AuthController;
 use App\Http\Middleware\EnsureIDIsValid;
 use Illuminate\Support\Facades\Route;
 
@@ -31,5 +32,22 @@ Route::post('/asignatura', [AsignaturaController::class, 'store']);     // Crear
 Route::put('/asignatura/{id}', [AsignaturaController::class, 'update'])->middleware(EnsureIDIsValid::class);; // Modificar
 Route::delete('/asignatura/{id}', [AsignaturaController::class, 'destroy']); // Borrar
 Route::delete('/asignatura_profesor/{id}', [AsignaturaController::class, 'showProfesor']); // Mostrar asignatura con profesor
+
+
+// Rutas públicas
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/public-endpoint', function () {
+    return response()->json(['message' => 'Esta es una ruta pública']);
+    });
+
+    Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'userProfile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+
 
 
